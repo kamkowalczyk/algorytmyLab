994a5227ff8ccad042c9e6974ce4a3b68caf1fbe
kamkowalczyk <kamil.kowalczyk890@gmail.com>
2022-10-02 15:32:56 +0200

    intest, outtest

---
+++ INOUTEST - Enormous Input and Output Test/INOUTEST - Enormous Input and Output Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace INOUTEST___Enormous_Input_and_Output_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ INTEST - Enormous Input Test/INTEST - Enormous Input Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace INTEST___Enormous_Input_Test
+{
+    class Program
+    {
+        public static void Main()
+        {
+            //Input
+            /*
+            string[] parts = Console.ReadLine().Split();
+            uint n = UInt32.Parse(parts[0]);
+            uint k = UInt32.Parse(parts[1]);
+            uint ans = 0;
+
+            for (uint i = 0; i < n; i++)
+            {
+                uint t = UInt32.Parse(Console.ReadLine());
+
+                if (t % k == 0)
+                    ans++;
+            }
+
+            Console.WriteLine(ans);
+            */
+
+            /*---------------------------------------------------------*/
+
+
+
+        }
+    }
+}
