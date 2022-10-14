3fba24e11faa7cad38ada6e29a57f1195dbeae92
kamkowalczyk <kamil.kowalczyk890@gmail.com>
2022-10-14 19:36:07 +0200

    newTasks

---
+++ ARRAYSUB - subarrays/ARRAYSUB - subarrays/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public static class ARRAYSUB
+{
+
+
+    public static int[] Solve(int[] array, int k)
+    {
+        if (k == array.Length) return new[] { array.Max() };
+        if (k == 1) return array;
+
+
+        var leftLookingDominators = new LinkedList<int>();
+        leftLookingDominators.AddFirst(array[k - 1]);
+
+        for (int i = k - 2; i >= 0; --i)
+        {
+            if (array[i] >= leftLookingDominators.Last.Value)
+            {
+                leftLookingDominators.AddLast(array[i]);
+            }
+        }
+
+
+        int subarrayCount = array.Length - k + 1;
+        int[] subarrayMaximums = new int[subarrayCount];
+        subarrayMaximums[0] = leftLookingDominators.Last.Value;
+
+        for (int i = 1, j = k; i < subarrayCount; ++i, ++j)
+        {
+            int lostLeftValue = array[i - 1];
+            int gainedRightValue = array[j];
+
+            if (lostLeftValue == leftLookingDominators.Last.Value)
+            {
+                leftLookingDominators.RemoveLast();
+            }
+
+            if (gainedRightValue > leftLookingDominators.Last.Value)
+            {
+                leftLookingDominators.Clear();
+                leftLookingDominators.AddFirst(gainedRightValue);
+            }
+            else
+            {
+                while (gainedRightValue > leftLookingDominators.First.Value)
+                {
+                    leftLookingDominators.RemoveFirst();
+                }
+                leftLookingDominators.AddFirst(gainedRightValue);
+            }
+
+            subarrayMaximums[i] = leftLookingDominators.Last.Value;
+        }
+
+        return subarrayMaximums;
+    }
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        int arrayLength = int.Parse(Console.ReadLine());
+        int[] array = Array.ConvertAll(
+            Console.ReadLine().Split(default(char[]), StringSplitOptions.RemoveEmptyEntries),
+            int.Parse);
+        int k = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(
+            string.Join(" ", ARRAYSUB.Solve(array, k)));
+    }
+}
---
+++ HOTELS - Hotels Along the Croatian Coast/HOTELS - Hotels Along the Croatian Coast/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace HOTELS
+{
+
+    public static class HOTELS
+    {
+        public static long Solve(int hotelCount, int costLimit, int[] hotelCosts)
+        {
+            int startIndex = 0, endIndex = 0;
+            long costFromStartToEnd = hotelCosts[0];
+            long maximumCostFromStartToEnd = 0;
+
+            while (true)
+            {
+               
+                while (endIndex + 1 < hotelCount
+                    && costFromStartToEnd + hotelCosts[endIndex + 1] <= costLimit)
+                {
+                    costFromStartToEnd += hotelCosts[++endIndex];
+                }
+
+                
+                if (costFromStartToEnd < costLimit)
+                {
+                    maximumCostFromStartToEnd = Math.Max(maximumCostFromStartToEnd, costFromStartToEnd);
+                }
+                else if (costFromStartToEnd == costLimit)
+                    return costLimit; 
+
+                if (endIndex + 1 == hotelCount)
+                    break;
+
+              
+                costFromStartToEnd -= hotelCosts[startIndex++];
+                if (startIndex > endIndex)
+                {
+                    endIndex = startIndex;
+                    costFromStartToEnd = hotelCosts[startIndex];
+                }
+            }
+            return maximumCostFromStartToEnd;
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] line = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+            int hotelCount = line[0];
+            int costLimit = line[1];
+
+            int[] hotelCosts = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+
+            Console.WriteLine(
+               HOTELS.Solve(hotelCount, costLimit, hotelCosts));
+        }
+    }
+}
